e1a2ec057ae02f63802fc4416cffae85368a9068
Ayodeji O <97124713+aosasona@users.noreply.github.com>
2024-06-29 22:42:04 +0100

    Added new config options and test to account for new targets

--- config/config.go
+++ config/config.go
@@ -4,6 +4,27 @@ import (
 	"fmt"
 )
 
+type Indentation int
+
+const (
+	emptyIndent Indentation = iota
+	Space
+	Tab
+)
+
+type Language string
+
+const (
+	LanguageTypescript Language = "typescript"
+	LanguageSwift      Language = "swift"
+)
+
+type Target struct {
+	Filename  string
+	Directory string
+	Language  Language
+}
+
 // Debug is a global variable that can be used to enable or disable debug mode
 var Debug = false
 
@@ -13,12 +34,15 @@ func SetDebug(v bool) {
 
 // Pointers have been used here to make sure the user actually sets the value and not just uses the default value
 type Config struct {
-	Enabled               *bool   // can be used to disable or enable the generation of types, defaults to false
-	OutputFile            *string // if nil, will default to types.ts in the current directory
-	UseTypeForObjects     *bool   // if true, will use `type Foo = ...` instead of `interface Foo {...}`, defaults to true
-	ExpandObjectTypes     *bool   // if true, will expand object types instead of just using the name (e.g foo: { bar: string } instead of foo: Bar)
-	PreferUnknown         *bool   // if true, will prefer unknown over any
-	AllowUnexportedFields *bool   // if true, will include private fields
+	Enabled               *bool    // can be used to disable or enable the generation of types, defaults to false
+	Targets               []Target // the targets to generate types for
+	UseTypeForObjects     *bool    // if true, will use `type Foo = ...` instead of `interface Foo {...}`, defaults to true
+	ExpandObjectTypes     *bool    // if true, will expand object types instead of just using the name (e.g foo: { bar: string } instead of foo: Bar)
+	PreferUnknown         *bool    // if true, will prefer unknown over any
+	AllowUnexportedFields *bool    // if true, will include private fields
+
+	IndentationType Indentation // the type of indentation to use, defaults to `Space`
+	SpaceCount      *int        // the number of spaces to use for indentation, defaults to 4
 
 	// TODO: implement custom types
 	CustomTypes map[string]string // custom types to be used in the generation of types
@@ -33,14 +57,6 @@ func (c Config) EnabledOrDefault() bool {
 	}
 
 	return *c.Enabled
-}
-
-func (c Config) OutputFileOrDefault() string {
-	if c.OutputFile == nil {
-		return "./types.ts"
-	}
-
-	return *c.OutputFile
 }
 
 func (c Config) ExpandObjectTypesOrDefault() bool {
@@ -75,19 +91,87 @@ func (c Config) AllowUnexportedFieldsOrDefault() bool {
 	return *c.AllowUnexportedFields
 }
 
+func (c Config) IndentationTypeOrDefault() Indentation {
+	if c.IndentationType == emptyIndent {
+		return Space
+	}
+
+	return c.IndentationType
+}
+
+func (c Config) SpaceCountOrDefault() int {
+	if c.SpaceCount == nil {
+		return 4
+	}
+
+	return *c.SpaceCount
+}
+
+func (c Config) GetIndentation() string {
+	indentationType := c.IndentationTypeOrDefault()
+	spaceCount := c.SpaceCountOrDefault()
+
+	if indentationType == Space {
+		space := ""
+		for i := 0; i < spaceCount; i++ {
+			space += " "
+		}
+
+		return space
+	}
+
+	tab := ""
+	repeatTabCount := int(spaceCount / 4)
+
+	for i := 0; i < repeatTabCount; i++ {
+		tab += "\t"
+	}
+
+	return tab
+}
+
+func (c Config) TargetsOrDefault() []Target {
+	if c.Targets == nil {
+		return []Target{}
+	}
+
+	return c.Targets
+}
+
 func (c Config) Merge(other Config) Config {
 	enabled := c.EnabledOrDefault()
-	outputFile := c.OutputFileOrDefault()
+	targets := c.TargetsOrDefault()
+
 	useTypeForObjects := c.UseTypeForObjectsOrDefault()
 	expandObjectTypes := c.ExpandObjectTypesOrDefault()
 	allowUnexportedFields := c.AllowUnexportedFieldsOrDefault()
+	preferUnknown := c.PreferUnknownOrDefault()
+
+	indentationType := c.IndentationTypeOrDefault()
+	spaceCount := c.SpaceCountOrDefault()
 
 	if other.Enabled != nil {
 		enabled = *other.Enabled
 	}
 
-	if other.OutputFile != nil {
-		outputFile = *other.OutputFile
+	if other.Targets != nil {
+		targets = append(targets, other.Targets...)
+
+		// Filter out duplicates
+		targetMap := make(map[string]Target)
+		for _, target := range targets {
+			targetMap[target.Filename] = target
+
+			if Debug {
+				fmt.Println("Added target", target.Filename)
+			}
+		}
+
+		targets = make([]Target, 0, len(targetMap))
+
+		for _, target := range targetMap {
+			targets = append(targets, target)
+		}
 	}
 
 	if other.UseTypeForObjects != nil {
@@ -102,11 +186,28 @@ func (c Config) Merge(other Config) Config {
 		allowUnexportedFields = *other.AllowUnexportedFields
 	}
 
+	if other.PreferUnknown != nil {
+		preferUnknown = *other.PreferUnknown
+	}
+
+	if other.IndentationType != emptyIndent {
+		indentationType = other.IndentationType
+	}
+
+	if other.SpaceCount != nil {
+		spaceCount = *other.SpaceCount
+	}
+
 	return Config{
-		Enabled:               &enabled,
-		OutputFile:            &outputFile,
+		Enabled: &enabled,
+		Targets: targets,
+
 		UseTypeForObjects:     &useTypeForObjects,
 		ExpandObjectTypes:     &expandObjectTypes,
 		AllowUnexportedFields: &allowUnexportedFields,
+		PreferUnknown:         &preferUnknown,
+
+		IndentationType: indentationType,
+		SpaceCount:      &spaceCount,
 	}
 }
