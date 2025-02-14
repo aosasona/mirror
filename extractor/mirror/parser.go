a07cc09a2a92a72c2b725de85921781ebedd82d2
Ayodeji O <97124713+aosasona@users.noreply.github.com>
2025-02-14 00:18:44 +0000

    Naive parser impl
    Frankly, this will be a silly approach since we will have unknown amount
    of things to parse since users can adn will stick anything they want in
    these tags; be it unclosed braces (for some reason) or even characters
    we haven't thought of

--- extractor/mirror/parser.go
+++ extractor/mirror/parser.go
@@ -1,5 +1,7 @@
 package mirrormeta
 
+import "fmt"
+
 func ref[T any](t T) *T {
 	return &t
 }
@@ -49,18 +51,125 @@ func NewMetaParser(input string) *MetaParser {
 }
 
 func (p *MetaParser) Parse() (*ParsedMeta, error) {
+	for p.currentIndex < len(p.chars) {
+		switch {
+		case isWhitespace(p.char()):
+			p.next()
+
+		// Read the attribute name - this put us in a parsing state
+		case isAlphaNumeric(p.char()) && p.currentAttribute == AtributeNone:
+			identifier := p.readIdentifier()
+			p.currentAttribute = p.getAttributeFromString(identifier)
+
+		default:
+			// If it is not an alphanumeric character and we are not parsing a node already, then it is an error
+			if p.currentAttribute == AtributeNone {
+				return nil, fmt.Errorf(
+					"unexpected character: %c, expected one of `type`, `name`, `skip`, `optional`",
+					p.char(),
+				)
+			}
+
+			var value string
+			// This tracks whether we are in a block like `type:{ ... }`
+			for p.currentIndex < len(p.chars) {
+				// Handle the type attribute first as it is the most complex bit
+				if p.currentAttribute == AtributeType {
+					// If it is a { [, (, or <, then we are in a block, read till we find the closing block
+					if isBlockStart(p.char()) {
+						value = p.readBlock()
+						break
+					}
+
+					// If it is not a block, then read till we find a comma or whitespace
+					start := p.currentIndex
+					for !isWhitespace(p.char()) && p.char() != ',' {
+						p.next()
+					}
+
+					value = string(p.chars[start:p.currentIndex])
+
+					// If we find a comma, then we are done reading the value
+					if p.char() == ',' {
+						p.next()
+						break
+					}
+				}
+			}
+		}
+	}
+
 	panic("TODO: implement")
 }
 
-func (p *MetaParser) setCurrentAttribute(text string) {
+func (p *MetaParser) readIdentifier() string {
+	start := p.currentIndex
+	for isAlphaNumeric(p.char()) {
+		p.next()
+	}
+
+	return string(p.chars[start:p.currentIndex])
+}
+
+func (p *MetaParser) readBlock() string {
+	start := p.currentIndex
+
+	for p.char() != '}' && p.char() != ']' && p.char() != ')' && p.char() != '>' {
+		p.next()
+	}
+
+	return string(p.chars[start:p.currentIndex])
+}
+
+func (p *MetaParser) char() rune {
+	if p.currentIndex >= len(p.chars) {
+		return 0
+	}
+
+	return p.chars[p.currentIndex]
+}
+
+func (p *MetaParser) peek() rune {
+	if p.currentIndex+1 >= len(p.chars) {
+		return 0
+	}
+
+	return p.chars[p.currentIndex+1]
+}
+
+func (p *MetaParser) next() rune {
+	if p.currentIndex >= len(p.chars) {
+		return 0
+	}
+
+	p.currentIndex++
+	return p.chars[p.currentIndex]
+}
+
+func (p *MetaParser) getAttributeFromString(text string) Attribute {
+	var attribute Attribute
 	switch text {
 	case "name":
-		p.currentAttribute = AtributeName
+		attribute = AtributeName
 	case "type":
-		p.currentAttribute = AtributeType
+		attribute = AtributeType
 	case "skip":
-		p.currentAttribute = AttributeSkip
+		attribute = AttributeSkip
 	case "optional":
-		p.currentAttribute = AtributeOptional
+		attribute = AtributeOptional
 	}
+
+	return attribute
+}
+
+func isWhitespace(r rune) bool {
+	return r == ' ' || r == '\n' || r == '\t'
+}
+
+func isAlphaNumeric(r rune) bool {
+	return (r >= 'a' && r <= 'z') || (r >= 'A' && r <= 'Z') || (r >= '0' && r <= '9')
+}
+
+func isBlockStart(r rune) bool {
+	return r == '{' || r == '[' || r == '(' || r == '<'
 }
